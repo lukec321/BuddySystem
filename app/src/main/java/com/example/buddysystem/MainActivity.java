55153d8e343d05d87d4222872f6b5f26bdcb68ab
lukec321 <luka.janezic@hotmail.com>
2022-01-09 20:50:34 +0100

    added volley reference.

--- app/src/main/java/com/example/buddysystem/MainActivity.java
+++ app/src/main/java/com/example/buddysystem/MainActivity.java
@@ -4,6 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
